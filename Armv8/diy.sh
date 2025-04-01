ae14f70e2a5dceb5aeb9f23adaafadd82c7de264
welvery2010 <very20101@users.noreply.github.com>
2025-04-01 16:03:10 +0800

    Update diy.sh

--- Armv8/diy.sh
+++ Armv8/diy.sh
@@ -11,6 +11,7 @@ git clone -b main https://github.com/very20101/openwrt_N1-Nova packages/opwrt_N1
 
 # Update packages
 rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf package/feeds/packages/rclone package/feeds/packages/aria2
 cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall feeds/luci/applications/
 
 ## add extra-package
@@ -20,6 +21,10 @@ cp -rf packages/opwrt_N1/extra-package/shadow-tls feeds/packages/shadow-tls
 cp -rf packages/opwrt_N1/extra-package/shadowsocksr-libev feeds/packages/shadowsocksr-libev
 cp -rf packages/opwrt_N1/extra-package/shadowsocks-libev feeds/packages/shadowsocks-libev
 cp -rf packages/opwrt_N1/extra-package/luci-app-adguardhome feeds/packages/luci-app-adguardhome
+
+cp -rf packages/opwrt_N1/extra-package/rclone package/feeds/packages/rclone
+cp -rf packages/opwrt_N1/extra-package/aria2 package/feeds/packages/aria2
+
 
 # Clean packages
 rm -rf clone
